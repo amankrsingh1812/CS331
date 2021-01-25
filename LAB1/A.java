a3f0f40eadd25f2a13ff825c986cd612cd742643
aman <amankrsingh1812@gmail.com>
2021-01-25 15:10:30 +0530

    Minor Changes

--- LAB1/A.java
+++ LAB1/A.java
@@ -1,4 +1,5 @@
-package MonteCarloSimulationFile;
+package PartA;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -8,96 +9,99 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.concurrent.TimeUnit;
 
-class workerThreadTask extends Thread{
-	private int Niterations;
+class workerThreadTask extends Thread {
+	private int Niterations; // Number of iterations required to be performed
 
-	public workerThreadTask(int Niterations)
-	{
-		this.Niterations=Niterations;
+	public workerThreadTask(int Niterations) {
+		this.Niterations = Niterations;
 	}
 
-	public void run()
-	{
+	// Each thread stores the count of points(both Inside circle and total points)
+	// in a local varible
+	// Finally it updates the global count of points
+	public void run() {
 		Random rand = new Random();
-		int localCirclePointsCount = 0;
-		int localSquarePointCount =0;
-		while(Niterations>0)
-		{
-			double x = rand.nextDouble();
+		int localCirclePointsCount = 0; // stores count of Points inside circle locally in the threads context
+		int localSquarePointCount = 0; // stores total count of Points locally in the threads context
+		while (Niterations > 0) {
+			double x = rand.nextDouble(); // Generate a random double value between 0 and 1 (inclusive)
 			double y = rand.nextDouble();
-			//System.out.println(x+" "+y);
 
-			double d=x*x+y*y;
-			if(d<=1)
+			double d = x * x + y * y; // Calcutate distance of points from origin
+
+			// Check whether generated points lie inside circle
+			if (d <= 1)
 				localCirclePointsCount++;
 			localSquarePointCount++;
 			Niterations--;
 		}
 
+		System.out.println(localCirclePointsCount);
+		// Update global counters
 		MonteCarloSimulation.circlePointsCount.getAndAdd(localCirclePointsCount);
 		MonteCarloSimulation.squarePointsCount.getAndAdd(localSquarePointCount);
 	}
 }
 
-class MonteCarloSimulation{
-	public static AtomicInteger circlePointsCount;
-	public static AtomicInteger squarePointsCount;
-	private static final int Npoints = 1000000;
+class MonteCarloSimulation {
+	public static AtomicInteger circlePointsCount; // Global counter of Points inside circle
+	public static AtomicInteger squarePointsCount; // Global counter of total number of Points generated
+	private static final int Npoints = 100000000; // Total Number of Points to be tested
 
-	private static int stringToInt(String s)
-	{
-		if(s.length() > 2)
+	// Helper function to convert String to Int. Return -1 in case of wrong input
+	private static int stringToInt(String s) {
+		if (s.length() > 2)
 			return -1;
-		int ans=0;
-		for(char c : s.toCharArray())
-		{
-			if(c-'0'<0||c-'0'>9)
+		int ans = 0;
+		for (char c : s.toCharArray()) {
+			if (c - '0' < 0 || c - '0' > 9)
 				return -1;
-			ans=ans*10;
-			ans=ans+(c-'0');
+			ans = ans * 10;
+			ans = ans + (c - '0');
 		}
 		return ans;
 	}
-	public static void main(String args[])
-	{
-		if(args.length != 1)
-		{
+
+	public static void main(String args[]) {
+		// Valid Input argument checks
+		if (args.length != 1) {
 			System.out.println("Please Enter Valid Number of arguments!!");
 			System.exit(1);
 		}
-		int Nthreads = stringToInt(args[0]);
+		int Nthreads = stringToInt(args[0]); // Stores Number of Threads from Input
 
-		if(Nthreads<4||Nthreads>16)
-		{
+		// Number of Threads Check
+		if (Nthreads < 4 || Nthreads > 16) {
 			System.out.println("Please Enter Valid Number of Threads!!");
 			System.exit(1);
 		}
 
-		circlePointsCount=new AtomicInteger(0);
-		squarePointsCount=new AtomicInteger(0);
+		// Initailise both global counter with 0
+		circlePointsCount = new AtomicInteger(0);
+		squarePointsCount = new AtomicInteger(0);
 
+		// Thread Executor Service of a fixed size Thread pool
 		ExecutorService taskExecutorService = Executors.newFixedThreadPool(Nthreads);
 
-		int perThreaditerations = (Npoints+Nthreads-1)/Nthreads;
-
-		for(int i=0;i<Nthreads;i++)
-		{
-			int numIterations = Math.min((i+1)*perThreaditerations,Npoints) - i*perThreaditerations;
+		// Give each thread Proportionate work by distributing almost equall number of iterations to each thread
+		int perThreaditerations = (Npoints + Nthreads - 1) / Nthreads;
+		for (int i = 0; i < Nthreads; i++) {
+			int numIterations = Math.min((i + 1) * perThreaditerations, Npoints) - i * perThreaditerations;
 			taskExecutorService.submit(new workerThreadTask(numIterations));
 		}
 
+		// Wait for task of all threads to complete
 		taskExecutorService.shutdown();
 		try {
 			taskExecutorService.awaitTermination(90, TimeUnit.SECONDS);
-		}
-		catch (InterruptedException e){
+		} catch (InterruptedException e) {
 			System.out.println("Fatal Error, Please run again!!");
 			System.exit(1);
 		}
 
-		double PI = (4*circlePointsCount.doubleValue())/squarePointsCount.doubleValue();
+		// Calcutae PI as Ratio of 4*NumberOfPointsInsideCircle / TotalNumberOfPoints
+		double PI = (4 * circlePointsCount.doubleValue()) / squarePointsCount.doubleValue();
 
-		System.out.println("The Estimated Value of PI is: "+PI);
+		System.out.println("The Estimated Value of PI is: " + PI);
 	}
 }
-
--- LAB1/C.java
+++ LAB1/C.java
@@ -1,4 +1,5 @@
-package matrixmultiplication;
+package PartC;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.Random;
@@ -7,181 +8,151 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
-class rowComputeTask extends Thread{
+// Thread task to compute a paticular row of result matrix
+class rowComputeTask extends Thread {
 	private int row;
 
-	public rowComputeTask(int row)
-	{
-		this.row=row;
+	public rowComputeTask(int row) {
+		this.row = row;
 	}
 
-	public void run()
-	{
-		for(int column=0;column<MatrixMultiplication.N;column++)
-		{
-			for(int j=0;j<MatrixMultiplication.N;j++)
-				MatrixMultiplication.C[row][column]+=MatrixMultiplication.A[row][j]*MatrixMultiplication.B[j][column];
+	public void run() {
+
+		// Runs in O(N*N)
+		for (int column = 0; column < MatrixMultiplication.N; column++) {
+			for (int j = 0; j < MatrixMultiplication.N; j++)
+				MatrixMultiplication.C[row][column] += MatrixMultiplication.A[row][j]
+						* MatrixMultiplication.B[j][column];
 		}
 	}
 }
 
-class initialiseTask extends Thread{
+// Thread task to initialise matrix
+// Initialises rows in [startRow,endRow-1]
+class initialiseTask extends Thread {
 	private int startRow;
 	private int endRow;
 
-	public initialiseTask(int startRow,int endRow)
-	{
+	public initialiseTask(int startRow, int endRow) {
 		this.startRow = startRow;
 		this.endRow = endRow;
 	}
 
-	public void run()
-	{	
+	public void run() {
 		Random rand = new Random();
-		
-		for(int row = startRow;row < endRow;row++)
-			for(int column = 0;column < MatrixMultiplication.N;column++)
-			{
-				MatrixMultiplication.C[row][column]=0;
-				MatrixMultiplication.A[row][column]=rand.nextInt(11);
-				MatrixMultiplication.B[row][column]=rand.nextInt(11);
+
+		for (int row = startRow; row < endRow; row++)
+			for (int column = 0; column < MatrixMultiplication.N; column++) {
+				MatrixMultiplication.C[row][column] = 0;
+				// Generate a Random Intergers between 0 to 10 (inclusive) for matrix A and B
+				MatrixMultiplication.A[row][column] = rand.nextInt(11);
+				MatrixMultiplication.B[row][column] = rand.nextInt(11);
 			}
 	}
 }
 
-class task1 extends Thread{
-	private int row;
-	private int column;
-
-	task1(int row,int column)
-	{
-		this.row=row;
-		this.column=column;
-	}
-
-	public void run()
-	{
-		for(int j=0;j<MatrixMultiplication.N;j++)
-		{
-			MatrixMultiplication.C[row][column]+=MatrixMultiplication.A[row][j]*MatrixMultiplication.B[j][column];
-		}
-	}
-}
-
-class MatrixMultiplication{
-	public static final int N=1000;
+class MatrixMultiplication {
+	public static final int N = 1000;
 	public static int A[][];
 	public static int B[][];
-	public static int C[][];
+	public static int C[][]; // Result Matrix
 	private static ExecutorService matrixMultiplicationExecutor;
 
-	private static int stringToInt(String s)
-	{
-		if(s.length() > 2)
+	private static int stringToInt(String s) {
+		if (s.length() > 2)
 			return -1;
-		int ans=0;
-		for(char c : s.toCharArray())
-		{
-			if(c-'0'<0||c-'0'>9)
+		int ans = 0;
+		for (char c : s.toCharArray()) {
+			if (c - '0' < 0 || c - '0' > 9)
 				return -1;
-			ans=ans*10;
-			ans=ans+(c-'0');
+			ans = ans * 10;
+			ans = ans + (c - '0');
 		}
 		return ans;
 	}
 
-	private static void intialise(int Nthreads)
-	{
+	private static void intialise(int Nthreads) {
 		A = new int[N][N];
 		B = new int[N][N];
 		C = new int[N][N];
 
-		int perThreadRows = (MatrixMultiplication.N+Nthreads-1)/Nthreads;
-		for(int i=0;i<Nthreads;i++)
-			matrixMultiplicationExecutor.submit(new initialiseTask(i*perThreadRows,Math.min((i+1)*perThreadRows,MatrixMultiplication.N)));
+		int perThreadRows = (MatrixMultiplication.N + Nthreads - 1) / Nthreads;
+		for (int i = 0; i < Nthreads; i++)
+			matrixMultiplicationExecutor.submit(
+					new initialiseTask(i * perThreadRows, Math.min((i + 1) * perThreadRows, MatrixMultiplication.N)));
 	}
 
-	public static void outputFile()
-	{
+	// Function to write the contents of Matrix C in a File
+	public static void outputFile() {
 		PrintStream o;
-        try {
-            o = new PrintStream(new File("MultiplicationOutput.txt"));
-            System.setOut(o);
-            System.setErr(o);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+		try {
+			o = new PrintStream(new File("MultiplicationOutput.txt"));
+			System.setOut(o);
+			System.setErr(o);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
-		for(int i=0;i<N;i++)
-		{
-			for(int j=0;j<N;j++)
-			{
-				System.out.print(C[i][j]+" ");
-		
-				//for(int k=0;k<N;k++)
-					//C[i][j]-=A[i][k]*B[k][j];
-
-				//if(C[i][j]!=0)
-				//System.out.println("Error at "+i+" "+j);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(C[i][j] + " ");
 			}
 			System.out.println("");
 		}
 	}
 
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		boolean fileOP = false;
-		if(args.length != 1 && args.length != 2)
-		{
+		if (args.length != 1 && args.length != 2) {
 			System.out.println("Please Enter Valid Number of arguments!!");
 			System.exit(1);
 		}
-		if(args.length == 2)
-		{
+		if (args.length == 2) {
 			int tval = stringToInt(args[1]);
-			if(tval <0 || tval > 1)
-			{
+			if (tval < 0 || tval > 1) {
 				System.out.println("Please Enter Valid Flags!!");
 				System.exit(1);
 			}
-			if(tval == 1)
+			if (tval == 1)
 				fileOP = true;
 		}
 		int Nthreads = stringToInt(args[0]);
 
-		if(Nthreads<4||Nthreads>16)
-		{
+		if (Nthreads < 4 || Nthreads > 16) {
 			System.out.println("Please Enter Valid Number of Threads!!");
 			System.exit(1);
 		}
 
+		// Create fixed size thread pool for initialisation
 		matrixMultiplicationExecutor = Executors.newFixedThreadPool(Nthreads);
+		// Initialise Matrices
 		intialise(Nthreads);
+
+		// Wait untill all entries in matrices are initialised
 		matrixMultiplicationExecutor.shutdown();
 		try {
 			matrixMultiplicationExecutor.awaitTermination(90, TimeUnit.SECONDS);
-		}
-		catch (InterruptedException e){
+		} catch (InterruptedException e) {
 			System.out.println("Fatal Error, Please run again!!");
 			System.exit(1);
 		}
 
-
+		// Create fixed size thread pool for multiplication
 		matrixMultiplicationExecutor = Executors.newFixedThreadPool(Nthreads);
 
-		for(int i=0;i<N;i++)
+		// 
+		for (int i = 0; i < N; i++)
 			matrixMultiplicationExecutor.submit(new rowComputeTask(i));
 
 		matrixMultiplicationExecutor.shutdown();
 		try {
 			matrixMultiplicationExecutor.awaitTermination(90, TimeUnit.SECONDS);
-		}
-		catch (InterruptedException e){
+		} catch (InterruptedException e) {
 			System.out.println("Fatal Error, Please run again!!");
 			System.exit(1);
 		}
 
-		if(fileOP)
+		if (fileOP)
 			outputFile();
 
 	}
